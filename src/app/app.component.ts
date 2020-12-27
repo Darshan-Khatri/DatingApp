9251a69cc640bb39fda8b0d62f6304ae0d2141b4
Darshan-Khatri <darshankhatri2297@gmail.com>
2020-12-26 20:13:57 -0600

    Section3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DatingApp';
+  users: any;
+
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.GetUser();
+  }
+
+  GetUser = () =>{
+    this.http.get('http://localhost:50569/api/Users').subscribe((feedback) =>{
+      console.log("FeedBack", feedback);
+      this.users = feedback;
+    },
+    err => {
+      console.log("Error", err);
+    }
+    )
+  }
 }
